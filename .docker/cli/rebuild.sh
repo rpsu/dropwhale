8780914cddf0f7d59520a859de944e868ee8a20b
socketwench <tess.flynn@ffwagency.com>
2016-03-24 11:56:12 -0500

    Rewritten CLI dockerfile and rebuild.sh to use Drupal Console instead.

--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -3,7 +3,23 @@
 /root/scripts/init.sh
 
 mkdir --parents --mode=777 /var/www/sites/default/files
-drush si --uri=http://web --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y
+drupal site:install \
+    --uri=http://web \
+    --root=/var/www \
+    --langcode=en \
+    --db-type=mysql \
+    --db-user=${DB_1_ENV_MYSQL_USER} \
+    --db-pass=${DB_ENV_MYSQL_PASS} \
+    --db-host=db \
+    --db-port=3306 \
+    --db-name=${DB_1_ENV_MYSQL_DB} \
+    --site-name='Drupal 8 Quick Start' \
+    --site-mail=admin@example.com \
+    --account-name=admin \
+    --account-mail=admin@example.com \
+    --account-pass=admin \
+    --no-interaction \
+    standard
 
 mkdir --parents --mode=777 /var/www/sites/simpletest
-drush --uri=http://web --root=/var/www en -y simpletest
+drupal module:install simpletest --uri=http://web --root=/var/www
